1f731a4c55ea773a590caec223706e77565773d4
Victoria Smith <victoriasmithh428@gmail.com>
2021-07-06 20:01:42 -0400

    create class component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
-import reactDom from "react-dom";
-
-const App = () => {
-    return <div>
-            Hi 
-        </div>
-    
+import ReactDOM from "react-dom";
+class App extends React.Component {
+    render(){
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position),
+            (err) => console.log(err)
+        )
+        return <div>Latitude: </div>
+    }
 }
 
 ReactDOM.render(<App/>, document.querySelector('#root'))
