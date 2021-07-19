dc40a26d4fc5d3ba808aa6944399831707c7c0f0
Victoria Smith <victoriasmithh428@gmail.com>
2021-07-19 19:10:30 -0400

    get user location

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 class App extends React.Component {
-    render(){
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (err) => console.log(err)
-        )
-        return <div>Latitude: </div>
-    }
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null };
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => console.log(err)
+    );
+  }
+
+  render() {
+    return <div>Latitude: {this.state.lat} </div>;
+  }
 }
 
-ReactDOM.render(<App/>, document.querySelector('#root'))
+ReactDOM.render(<App />, document.querySelector("#root"));
